76dea7d034676df1e7f1ae4aa2d18de8b8475491
Jennifer Pazos <jmp521@gmail.com>
2018-11-26 02:09:43 -0600

    added routes for deposit and withdrawal and add deposit action

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -7,13 +7,37 @@ class TransactionsController < ApplicationController
   end
 
 
-  def create
+  # def create
+  #   binding.pry
+  #
+  #   @transaction = Transaction.create(transaction_params)
+  # end
 
-    @transaction = Transaction.create(transaction_params)
+  def withdrawal
+
+  end
+
+  def deposit
+    #find the coin that you want to increment
+    #increase the quantity of the coin
+    
+    @coin = Coin.find_by(:id => params[:coin_id])
+    if @coin
+      @coin.quantity += 1
+      @coin.save
+      @transaction = Transaction.create(:coin_id => @coin.id, :api_user_id => @current_user.id, :deposit_or_withdrawal => "deposit")
+      if @transaction
+        render json: "Successfully deposited coin"
+      else
+        render json: "Your deposit was rejected"
+      end
+    else
+      render json: "Coin is not found"
+    end
+
   end
 
   def by_user
-    binding.pry
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   get 'coins/total' => "coins#total"
   get 'transactions/by_user' => "transactions#by_user"
+  post '/transactions/deposit' => "transactions#deposit"
+  post '/transactions/withdrawal' => "transactions#withdrawal"
   resources :api_users
-  resources :transactions
+  resources :transactions, only: [:index, :show, :create]
   resources :coins
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
