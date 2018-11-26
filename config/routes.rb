498a00958ec079e7dec529da4d282ffdd0b1d8f9
Jennifer Pazos <jmp521@gmail.com>
2018-11-25 23:36:00 -0600

    added notes to readme about coin routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :api_users
   resources :transactions
   resources :coins
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
