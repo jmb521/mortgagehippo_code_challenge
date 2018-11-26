5ff15f3c516f0a512ae2f0d53a523481913df01e
Jennifer Pazos <jmp521@gmail.com>
2018-11-26 02:11:10 -0600

    updated serializer to reflect deposit_or_withdrawal transaction type

--- app/serializers/transaction_serializer.rb
+++ app/serializers/transaction_serializer.rb
@@ -1,5 +1,5 @@
 class TransactionSerializer < ActiveModel::Serializer
-  attributes :id, :coin_id, :api_user_id, :created_at
+  attributes :id, :coin_id, :api_user_id, :created_at, :deposit_or_withdrawal
   belongs_to :coin
-  
+
 end
