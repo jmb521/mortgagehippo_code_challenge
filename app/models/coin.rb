d74d3d279dbcbcaa523d541c16f0aceeb5bdb24f
Jennifer Pazos <jmp521@gmail.com>
2018-11-25 23:35:32 -0600

    added method to set quantity on creation

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -1,4 +1,11 @@
 class Coin < ApplicationRecord
   has_one :transactions
   has_one :users
+
+  after_create :set_default_quantity
+
+  def set_default_quantity
+    self.quantity = self.quantity | 1
   end
+
+end
