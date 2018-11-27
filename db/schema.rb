ba32d452b9e6dd74b9032ed5a17205d666bba4d1
Jennifer Pazos <jmp521@gmail.com>
2018-11-27 00:03:56 -0600

    added default value for quantity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_26_065157) do
+ActiveRecord::Schema.define(version: 2018_11_26_214618) do
 
   create_table "api_users", force: :cascade do |t|
     t.string "api_key"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2018_11_26_065157) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "quantity"
+    t.integer "quantity", default: 0
   end
 
   create_table "transactions", force: :cascade do |t|
