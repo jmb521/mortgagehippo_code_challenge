de0e1cf868610a808949eb336ce2e3f0b244f2c5
Jennifer Pazos <jmp521@gmail.com>
2018-11-27 01:34:11 -0600

    added admin resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_26_214618) do
+ActiveRecord::Schema.define(version: 2018_11_27_071125) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "api_users", force: :cascade do |t|
     t.string "api_key"
