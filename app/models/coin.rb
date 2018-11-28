97bbe6f2750dfd957fc07fbedf0949d3d1750055
Jennifer Pazos <jmp521@gmail.com>
2018-11-28 00:19:43 -0600

    moved the logic from the controller to the model so it can be accessed by the route and the mailer view

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -2,6 +2,15 @@ class Coin < ApplicationRecord
   has_one :transactions
   has_one :users
 
-  
+  def self.coin_total
+    total = 0
+    
+    Coin.all.each do |c|
+      total += c.value.to_f.round(2) * c.quantity.to_i
+      total = total.round(2)
+    end
+    total
+
+  end
 
 end
