708aa25ab05d0668235ca1577af3c09b25f8a012
Jennifer Pazos <jmp521@gmail.com>
2018-11-27 21:37:03 -0600

    added api key to application.rb

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,11 @@ module CoinMachine
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
+    config.action_mailer.delivery_method = :sendgrid_actionmailer
+    config.action_mailer.sendgrid_actionmailer_settings = {
+      api_key: ENV['SENDGRID_API_KEY'],
+      raise_delivery_errors: true
+}
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
